3e70a6528f932a356385d224191484ca3cc4eb50
David J. Burger <dburger@dell-wildcat.gateway.2wire.net>
2010-01-04 19:08:56 -0800

    add reload capability, keep window open on run

--- content_script.js
+++ content_script.js
@@ -1,11 +1,27 @@
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+var reload = function(responseHandler) {
+    window.location.href = window.location.href;
+    responseHandler(null);
+};
+var execute = function(code, responseHandler) {
     var exception = null;
     try {
-        eval(request.code);
+        eval(code);
     } catch (exc) {
       // there was a circular reference in jQueries exceptions that
       // sendResponse didn't like
       exception = {name: exc.name, message: exc.message};
     }
-    sendResponse(exception);
+    responseHandler(exception);
+};
+
+chrome.extension.onRequest.addListener(
+        function(data, sender, responseHandler) {
+    if (data.reload) {
+        reload(responseHandler);
+    } else if (data.code) {
+        execute(data.code, responseHandler);
+    } else {
+        responseHandler({name: "InvalidArgumentException",
+                message: "Argument to content script not understood."});
+    }
 });
